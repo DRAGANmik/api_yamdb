167a0f7a203df201b5b550a58e2231fe04237060
Sofya <sofya.s.frolova@gmail.com>
2021-04-22 22:50:28 +0300

    First commit C

--- reviews_comments/models.py
+++ reviews_comments/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews"
+    )
+    title= models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name="reviews"
+    )
+    pub_date = models.DateTimeField(
+        "Дата добавления", auto_now_add=True, db_index=True
+    )
+    score = models.IntegerField()
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
+    pub_date = models.DateTimeField(
+        "Дата добавления", auto_now_add=True, db_index=True
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name="comments"
+    )
