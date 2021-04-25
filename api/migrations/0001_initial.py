52316d7a61414655616002366cc2b4887bb440c2
Mike <mike94.0653@ya.ru>
2021-04-25 12:50:34 +0300

    modified

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -36,7 +36,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.TextField()),
                 ('year', models.DateTimeField(auto_now_add=True, verbose_name='Год публикации')),
-                ('rating', models.CharField(max_length=20)),
             ],
         ),
         migrations.CreateModel(
